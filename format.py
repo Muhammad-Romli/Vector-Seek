226dd8e44144a40a9310350f3cd4101781d02fe4
Muhammad Romli <genbugenba@gmail.com>
2026-08-23 22:07:18 +0700

    Experimenting on the formatting

--- format.py
+++ format.py
@@ -1,3 +1,7 @@
+import shutil
+import textwrap
+
+
 BOLD = "\033[1m"
 RESET = "\033[0m"
 GREEN = "\033[32m"
@@ -5,26 +9,28 @@ CYAN = "\033[36m"
 BRIGHT_GREEN = "\033[92m"
 
 def format_top_similarity(embed_metadatas: list[dict], num_shown: int):
+    width = shutil.get_terminal_size().columns
 
-
-    print("\n" * 10)
-    print("=" * 110)
+    print("\n" * 3)
+    print("=" * width)
     print(f"                    {CYAN}{BOLD}Result of similarities search from most similar to least:{RESET}                    ")
-    print("=" * 110)
+    print("=" * width)
     count = 0
     for embed_metadata in embed_metadatas[:num_shown]:
+        wrapped_text = textwrap.fill(embed_metadata["content"], width=100)
         count += 1
         print(f"""
 
         {BOLD}{GREEN}RESULT {count}{RESET}:
-{embed_metadata["content"]}
+{wrapped_text}
 
-{BRIGHT_GREEN}{BOLD}FROM_FILE:{RESET} {embed_metadata["metadata"]["from_file"]}
-{BRIGHT_GREEN}{BOLD}FROM_TITLE:{RESET} {embed_metadata["metadata"]["from_title"]}
-{BRIGHT_GREEN}{BOLD}CHUNK ID:{RESET} {embed_metadata["metadata"]["chunk_id"]}
+{BRIGHT_GREEN}{BOLD}f"{'FROM_FILE':<12}:"{RESET} {embed_metadata["metadata"]["from_file"]}
+{BRIGHT_GREEN}{BOLD}f"{'FROM_TITLE':<12}:"{RESET} {embed_metadata["metadata"]["from_title"]}
+{BRIGHT_GREEN}{BOLD}f"{'CHUNK ID':12}:"{RESET} {embed_metadata["metadata"]["chunk_id"]}
 
 
-{"_" * 110}
+{"_" * width}
 """)
+    print("\n" * 3)
 
 # embed_metadatas variable data, should look like wmbed_metadata like dataflow number 3 in README.md
